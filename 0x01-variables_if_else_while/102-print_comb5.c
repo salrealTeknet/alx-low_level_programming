28e468e52b8f1776f208d4c573cd356cdd1e713f
salrealTeknet <salihuabduljawwad.a@gmail.com>
2022-07-04 15:12:29 -0700

    corrections

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
-/*
- * File: 102-print_comb5.c
- */
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+*/
+
 int main(void)
 {
-	int num1, num2;
+	int p, q;
 
-	for (num1 = 0; num1 <= 98; num1++)
+	for (p = 0; p <= 98; p++)
 	{
-		for (num2 = num1 + 1; num2 <= 99; num2++)
+		for (q = p + 1; q <= 99; q++)
 		{
-			putchar((num1 / 10) + '0');
-			putchar((num1 % 10) + '0');
+			putchar((p / 10) + '0');
+			putchar((p % 10) + '0');
 			putchar(' ');
-			putchar((num2 / 10) + '0');
-			putchar((num2 % 10) + '0');
+			putchar((q / 10) + '0');
+			putchar((q % 10) + '0');
 
-			if (num1 == 98 && num2 == 99)
+			if (p == 98 && q == 99)
 				continue;
 
 			putchar(',');
