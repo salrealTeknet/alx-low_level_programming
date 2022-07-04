f1b81bd8a8c3dbcc50ab368e0d78d88f2878217a
salrealTeknet <salihuabduljawwad.a@gmail.com>
2022-07-04 15:05:40 -0700

    commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,29 +1,27 @@
 #include <stdio.h>
 
-/**
- * main - Print numbers from 00 to 99
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-	int tens;
-	int ones;
+	int num1, num2;
 
-	for (tens = 0; tens <= 9; tens++)
+	for (num1 = 0; num1 <= 98; num1++)
 	{
-		for (ones = 0; ones <= 9; ones++)
+		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
-			putchar(tens + '0');
-			putchar(ones + '0');
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
 
-			if (!(tens == 9 && ones == 9))
-			{
-				putchar(',')(',');
-				putchar(' ')(',');
-			}
+			if (num1 == 98 && num2 == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
 		}
 	}
+
 	putchar('\n');
 
 	return (0);
