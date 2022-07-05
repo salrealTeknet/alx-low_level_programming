38b49d7a511409dd9bfb10c83d34f74b80b1090e
salrealTeknet <salihuabduljawwad.a@gmail.com>
2022-07-05 14:56:50 -0700

    small letters

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,10 +2,10 @@
 
 /**
  * _islower - Checks if a character is lowercase.
- * @c: The character to be checked.
- *
- * Return: 1 if character is lowercase, 0 otherwise.
+ * 
+ *return (0);
  */
+
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
