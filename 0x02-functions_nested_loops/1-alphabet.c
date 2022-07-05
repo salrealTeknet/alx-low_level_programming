2dc2e6bacb979d98374b870183a219b9c7a1f8ee
salrealTeknet <salihuabduljawwad.a@gmail.com>
2022-07-05 14:31:32 -0700

    small letter

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,10 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ *print_alphabet - prints the alphabet in lowercase, followed by a new line.
  *
  * Return: nothing
  */
-
-/**
- * print_alphabet - prints the alphabet in lowercase, followed by a new line.
- */
-
 void print_alphabet(void)
 {
 	char letter;
