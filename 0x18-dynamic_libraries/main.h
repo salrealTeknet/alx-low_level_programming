bb32fabc73b456b78fb3062e67d67dcb089dd1dd
salrealTeknet <salihuabduljawwad.a@gmail.com>
2022-10-04 04:16:31 -0700

    Dynamic_library

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,6 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -21,5 +28,6 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
 
 #endif
